600443383ec4edb522e7af36bc255789b53205e3
jkubisiowski <jimjam.sg@gmail.com>
2020-10-17 13:43:51 +0200

    Simplify GA setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,17 +33,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: "G-RK3V3S5283",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,        
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,        
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10        
+        head: true     
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
